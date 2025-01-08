1a5455833b0875ec7af22cc4b9639ea815335453
lholzmair <l.holzmair@students.htl-leonding.ac.at>
2025-01-08 17:34:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // MySQL-Verbindung
 const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT || 3306,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
