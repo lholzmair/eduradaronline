05148af810dfcb1c0f707b871ffc382352de0b87
lholzmair <l.holzmair@students.htl-leonding.ac.at>
2025-01-08 18:01:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const next = require('next');
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const dev = process.env.NODE_ENV !== 'production'; // Entwicklungsmodus prüfen
